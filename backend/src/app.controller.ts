58a3ae0b93f4b94579d73f3087bbc6f60e322df2
Timotej Šulík <xsulik@fi.muni.cz>
2022-05-26 19:27:47 +0200

    fix: Revert accidental test change

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,28 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { PrismaClient } from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getHello(): Promise<string> {
-    const prisma = new PrismaClient();
-
-    const newUser = await prisma.user.create({
-      data: {
-        email: 'lukas.urbn@gmail.com',
-        name: 'lukasko',
-        surname: 'urbanko',
-        company: 'jo',
-        logo: 'google.com',
-      },
-    });
-
-    const users = await prisma.user.findMany();
-    console.log(users);
-
-    return 'User was created: ' + newUser.name;
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
