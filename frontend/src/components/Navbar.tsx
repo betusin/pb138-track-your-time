8933d0b42a178a648beabcf1d1012775bbe26bf4
Alžbeta Hajná <xhajna@fi.muni.cz>
2022-05-26 12:14:31 +0200

    feat: changed from vite init page to TrackYourTime

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export const Navbar = () => {
+  return (
+    <div>
+      <h1>TrackYourTime</h1>
+      This is Navbar, where one day will be <a href="#">links</a>. But not <a href="#">yet</a>.
+    </div>
+  );
+}
