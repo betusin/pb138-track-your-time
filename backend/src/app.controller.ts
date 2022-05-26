1b900b6877d9e914334103df32841d01abfd4931
Timotej Šulík <xsulik@fi.muni.cz>
2022-05-26 14:02:57 +0200

    build: Configure Docker

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaClient } from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    const prisma = new PrismaClient();
+
+    const newUser = await prisma.user.create({
+      data: {
+        email: 'lukas.urbn@gmail.com',
+        name: 'lukasko',
+        surname: 'urbanko',
+        company: 'jo',
+        logo: 'google.com',
+      },
+    });
+
+    const users = await prisma.user.findMany();
+    console.log(users);
+
+    return 'User was created: ' + newUser.name;
   }
 }
