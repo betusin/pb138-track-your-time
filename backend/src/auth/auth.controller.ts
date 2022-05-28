ca9a5e69d6f0ac4132fa0a9e0f0a8947875c3f61
Timotej Šulík <xsulik@fi.muni.cz>
2022-05-28 22:11:12 +0200

    feat(backend): Add email and password parameters to login Swagger UI

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { ApiTags } from '@nestjs/swagger';
+import { ApiImplicitQuery } from '@nestjs/swagger/dist/decorators/api-implicit-query.decorator';
 
 @ApiTags('Authentication')
 @Controller({ path: 'auth', version: '1' })
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiImplicitQuery({ name: 'email', type: String })
+  @ApiImplicitQuery({ name: 'password', type: String })
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
