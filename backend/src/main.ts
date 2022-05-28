a24e0a6e6f40c47b3b8f9dd1eb83f40d3e30c526
Timotej Šulík <xsulik@fi.muni.cz>
2022-05-28 18:13:53 +0200

    feat(backend): Set up Swagger UI in the project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,11 @@ async function bootstrap() {
   // Endpoint prefix and versioning
   app.setGlobalPrefix('/api');
   app.enableVersioning({ type: VersioningType.URI });
+
+  // Swagger UI
+  const config = new DocumentBuilder().setTitle('TrackYourTime').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/swagger', app, document);
 
   await app.listen(3000);
 }
