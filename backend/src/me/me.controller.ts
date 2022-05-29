14f860454ef9b0e00710a9c77a7212207fd5207c
Timotej Šulík <xsulik@fi.muni.cz>
2022-05-29 14:16:41 +0200

    feat(backend): Do not require user ID for management of the current user

--- backend/src/me/me.controller.ts
+++ backend/src/me/me.controller.ts
@@ -1,16 +1,27 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
   InternalServerErrorException,
+  Patch,
   Request,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UserService } from '../user/user.service';
 import { GetProjectDto } from '../project/dto/get-project.dto';
 import { ProjectService } from '../project/project.service';
 import { GetUserDto } from '../user/dto/get-user-dto.dto';
+import { UpdateUserDto } from '../user/dto/update-user.dto';
 
 @ApiTags('Me')
 @ApiBearerAuth('access-token')
@@ -39,5 +50,30 @@ export class MeController {
   @Get('/projects')
   async findAll(): Promise<GetProjectDto[]> {
     return this.projectService.findAll();
+  }
+
+  @ApiTags('Users')
+  @ApiNoContentResponse({ description: 'The user was updated' })
+  @ApiBadRequestResponse({ description: 'Field validation failed' })
+  @ApiNotFoundResponse({ description: 'The user was not found' })
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
+  @Patch()
+  async update(
+    @Request() req,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<void> {
+    await this.userService.update(req.user.userId, updateUserDto);
+  }
+
+  @ApiTags('Users')
+  @ApiOkResponse({ description: 'The user was deleted' })
+  @ApiBadRequestResponse({ description: 'Field validation failed' })
+  @ApiNotFoundResponse({ description: 'The user was not found' })
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
+  @Delete()
+  async remove(@Request() req): Promise<void> {
+    await this.userService.remove(req.user.userId);
   }
 }
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,26 +1,11 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Param,
-  ParseUUIDPipe,
-  Patch,
-  Post,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import {
   ApiBadRequestResponse,
-  ApiBearerAuth,
   ApiCreatedResponse,
-  ApiNoContentResponse,
-  ApiNotFoundResponse,
-  ApiOkResponse,
   ApiTags,
 } from '@nestjs/swagger';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @ApiTags('Users')
 @Controller({ path: 'users', version: '1' })
@@ -32,28 +17,5 @@ export class UserController {
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<void> {
     await this.userService.create(createUserDto);
-  }
-
-  @ApiNoContentResponse({ description: 'The user was updated' })
-  @ApiBadRequestResponse({ description: 'Field validation failed' })
-  @ApiNotFoundResponse({ description: 'The user was not found' })
-  @ApiBearerAuth('access-token')
-  @UseGuards(JwtAuthGuard)
-  @Patch(':id')
-  async update(
-    @Param('id', ParseUUIDPipe) id: string,
-    @Body() updateUserDto: UpdateUserDto,
-  ): Promise<void> {
-    await this.userService.update(id, updateUserDto);
-  }
-
-  @ApiOkResponse({ description: 'The user was deleted' })
-  @ApiBadRequestResponse({ description: 'Field validation failed' })
-  @ApiNotFoundResponse({ description: 'The user was not found' })
-  @ApiBearerAuth('access-token')
-  @UseGuards(JwtAuthGuard)
-  @Delete(':id')
-  async remove(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
-    await this.userService.remove(id);
   }
 }
