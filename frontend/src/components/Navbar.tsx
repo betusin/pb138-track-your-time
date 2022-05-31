9b845d75bcebbd9eaec6661b4b1b06156ec81c15
Alžbeta Hajná <xhajna@fi.muni.cz>
2022-05-31 21:59:13 +0200

    feat: added Link to Home page in Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom';
+
 export const Navbar = () => {
   return (
     <div>
       <h1>TrackYourTime</h1>
-      This is Navbar, where one day will be <a href="#">links</a>. But not <a href="#">yet</a>.
+      <Link to="/">Home</Link>
     </div>
   );
 }
