14a2cf8d6ad67dd85c30068a25c78dab79b2ab38
Alžbeta Hajná <xhajna@fi.muni.cz>
2022-05-31 21:02:24 +0200

    fix(frontend): link to Project page from project name, not whole projet item

--- frontend/src/components/ProjectItem.tsx
+++ frontend/src/components/ProjectItem.tsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 export const ProjectItem = ({id, name, hourly_rate, isActive, customer}: IProjectType) => {
   return (
-    <Link to={`/project/${id}`} className={`project-item ${!isActive && 'project-item--not-active'} p1 m1`}>
-      <div className="project-item__name"><strong>{name}</strong></div>
+    <div className={`project-item ${!isActive && 'project-item--not-active'} p1 m1`}>
+      <Link to={`/project/${id}`} className="project-item__name"><strong>{name}</strong></Link>
       <div className="project-item__customer">{customer}</div>
       <div className="project-item__hourly-rate">{hourly_rate}$/hour</div>
       <div className="project-item__settings">
         <img className="icon" src={`/assets/edit-${isActive ? 'antracit' : 'lime'}.svg`} />
         <img className="icon" src={`/assets/delete-${isActive ? 'antracit' : 'lime'}.svg`} />
       </div>
-    </Link>
+    </div>
   );
 }
