de50b4f4a2165014e3208034633b52af60d71d44
Alžbeta Hajná <xhajna@fi.muni.cz>
2022-06-01 10:35:55 +0200

    feat: delete icon in two states for not randomly deleting project

--- frontend/src/components/ProjectItem.tsx
+++ frontend/src/components/ProjectItem.tsx
@@ -1,7 +1,14 @@
 import { IProjectType } from "../types";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export const ProjectItem = ({id, name, hourly_rate, isActive, customer}: IProjectType) => {
+  const [ wantToRemove, setWantToRemove ] = useState(false);
+
+  const deleteProject= () => {
+    window.alert("deleting project with id " + id);
+  }
+
   return (
     <div className={`project-item ${!isActive && 'project-item--not-active'} p1 m1`}>
       <Link to={`/project/${id}`} className="project-item__name"><strong>{name}</strong></Link>
@@ -9,7 +16,11 @@ export const ProjectItem = ({id, name, hourly_rate, isActive, customer}: IProjec
       <div className="project-item__hourly-rate">{hourly_rate}$/hour</div>
       <div className="project-item__settings">
         <img className="icon" src={`/assets/edit-${isActive ? 'antracit' : 'lime'}.svg`} />
-        <img className="icon" src={`/assets/delete-${isActive ? 'antracit' : 'lime'}.svg`} />
+        {wantToRemove ? (
+          <img className={`icon icon--inverse ${isActive && 'icon--active_inverse'}`} title='Yes, I want to delete the project' onClick={() => deleteProject()} src={`/assets/delete-${isActive ? 'lime' : 'antracit'}.svg`} />
+        ) : (
+          <img className="icon" title='Delete the project' onClick={() => setWantToRemove(!wantToRemove)} src={`/assets/delete-${isActive ? 'antracit' : 'lime'}.svg`} />
+        )}
       </div>
     </div>
   );
