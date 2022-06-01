19c6a3867ca38481ec62196ca609225f19508636
Alžbeta Hajná <xhajna@fi.muni.cz>
2022-06-01 13:19:06 +0200

    feat: component for editing project and router to EditProject

--- frontend/src/RouterLinks.tsx
+++ frontend/src/RouterLinks.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import { CreateProject } from './components/CreateProject';
 import { CreateSession } from './components/CreateSession';
+import { EditProject } from './components/EditProject';
 import { Project } from "./components/Project";
 
 export const RouterLinks = () => {
@@ -10,6 +11,7 @@ export const RouterLinks = () => {
       <Routes>
         <Route path="/" element={<App />}/>
         <Route path="/project/add" element={<CreateProject />}/>
+        <Route path="/project/edit/:id" element={<EditProject />}/>
         <Route path="/project/:id" element={<Project />}/>
         <Route path="/session/add" element={<CreateSession />}/>
       </Routes>
--- frontend/src/components/ProjectItem.tsx
+++ frontend/src/components/ProjectItem.tsx
@@ -15,7 +15,9 @@ export const ProjectItem = ({id, name, hourly_rate, isActive, customer}: IProjec
       <div className="project-item__customer">{customer}</div>
       <div className="project-item__hourly-rate">{hourly_rate}$/hour</div>
       <div className="project-item__settings">
-        <img className="icon" src={`/assets/edit-${isActive ? 'antracit' : 'lime'}.svg`} />
+        <Link to ={`/project/edit/${id}`}>
+          <img className="icon" src={`/assets/edit-${isActive ? 'antracit' : 'lime'}.svg`} />
+        </Link>
         {wantToRemove ? (
           <img className={`icon icon--inverse ${isActive && 'icon--active_inverse'}`} title='Yes, I want to delete the project' onClick={() => deleteProject()} src={`/assets/delete-${isActive ? 'lime' : 'antracit'}.svg`} />
         ) : (
