6608aaa49c3aed600bda7bcadd071001163c19e8
Timotej Šulík <xsulik@fi.muni.cz>
2022-06-01 11:20:44 +0200

    feat(backend): Remember Authorization in Swagger UI

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -36,7 +36,11 @@ async function bootstrap() {
     )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api', app, document);
+  SwaggerModule.setup('/api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(3000);
 }
