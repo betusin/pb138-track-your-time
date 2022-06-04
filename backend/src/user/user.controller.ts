1f2927d6bd0d7ca08eed54af5a78e26d18bc1b4f
Timotej Šulík <xsulik@fi.muni.cz>
2022-06-04 12:12:48 +0200

    feat(backend): Add 409 response documentation for POST /users

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -3,6 +3,7 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import {
   ApiBadRequestResponse,
+  ApiConflictResponse,
   ApiCreatedResponse,
   ApiOperation,
   ApiTags,
@@ -16,6 +17,7 @@ export class UserController {
   @ApiOperation({ summary: 'Creates a new user account' })
   @ApiCreatedResponse({ description: 'The user was created' })
   @ApiBadRequestResponse({ description: 'Field validation failed' })
+  @ApiConflictResponse({ description: 'A user with this email already exists' })
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<void> {
     await this.userService.create(createUserDto);
