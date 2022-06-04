36922a1d16e09efb16e7fd6b47443d179432920a
Alžbeta Hajná <xhajna@fi.muni.cz>
2022-06-04 23:40:53 +0200

    feat: links from register/login to login/register

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,16 +1,14 @@
-import { SubmitHandler } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
-import { accessTokenAtom } from '../../state/atom';
-import { AuthForm, IFormAuthInput } from './AuthForm';
+import { SubmitHandler } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { accessTokenAtom } from "../../state/atom";
+import { AuthForm, IFormAuthInput } from "./AuthForm";
 
 export const Login = () => {
   const setToken = useSetRecoilState(accessTokenAtom);
   const navigate = useNavigate();
 
-  const onSubmit: SubmitHandler<IFormAuthInput> = (
-    data: IFormAuthInput
-  ) => {
+  const onSubmit: SubmitHandler<IFormAuthInput> = (data: IFormAuthInput) => {
     console.log(data);
     window.alert("you would be logged in");
 
@@ -23,6 +21,7 @@ export const Login = () => {
     <div className="App">
       <h1>TrackYourTime</h1>
       <AuthForm onSubmit={onSubmit} buttonText="Login" />
+      Don't have an account yet? Register <Link to="/register">here</Link>.
     </div>
   );
-}
+};
--- frontend/src/components/auth/Register.tsx
+++ frontend/src/components/auth/Register.tsx
@@ -1,20 +1,18 @@
-import { SubmitHandler } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
-import { accessTokenAtom } from '../../state/atom';
-import { AuthForm, IFormAuthInput } from './AuthForm';
+import { SubmitHandler } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { accessTokenAtom } from "../../state/atom";
+import { AuthForm, IFormAuthInput } from "./AuthForm";
 
 export const Register = () => {
   const setToken = useSetRecoilState(accessTokenAtom);
   const navigate = useNavigate();
 
-  const onSubmit: SubmitHandler<IFormAuthInput> = (
-    data: IFormAuthInput
-  ) => {
+  const onSubmit: SubmitHandler<IFormAuthInput> = (data: IFormAuthInput) => {
     console.log(data);
     window.alert("you would be registered in");
 
-    setToken('tokenTODO');
+    setToken("tokenTODO");
 
     navigate("/");
   };
@@ -23,6 +21,7 @@ export const Register = () => {
     <div className="App">
       <h1>TrackYourTime</h1>
       <AuthForm onSubmit={onSubmit} buttonText="Register" />
+      Already have an account? Login <Link to="/login">here</Link>.
     </div>
   );
-}
+};
