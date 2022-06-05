28447ba078a8d29a0cde7b8717b045316b67544a
Alžbeta Hajná <xhajna@fi.muni.cz>
2022-06-05 14:57:33 +0200

    feat: implemented registering

--- frontend/src/components/auth/Register.tsx
+++ frontend/src/components/auth/Register.tsx
@@ -1,6 +1,8 @@
 import { SubmitHandler } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
+import { authControllerLogin } from "../../api/authentication/authentication";
+import { userControllerCreate } from "../../api/users/users";
 import { accessTokenAtom } from "../../state/atom";
 import { IFormRegisterInput, RegisterForm } from "./AuthForm";
 
@@ -11,10 +13,26 @@ export const Register = () => {
   const onSubmit: SubmitHandler<IFormRegisterInput> = async (
     data: IFormRegisterInput
   ) => {
+    if (!data.logo) data.logo = "/assets/company-logo.svg";
 
-    setToken("tokenTODO");
-
-    navigate("/");
+    const result = await userControllerCreate({ ...data });
+    if (result.status == 201) {
+      const resultLogin = await authControllerLogin({ ...data });
+      if (resultLogin.status == 201) {
+        setToken(resultLogin.data.access_token);
+        navigate("/");
+      } else if (resultLogin.status == 401) {
+        alert("Incorrect email or password!");
+      } else {
+        alert("Unknown error: " + resultLogin.statusText);
+      }
+    } else if (result.status == 400) {
+      alert("Field validation failed");
+    } else if (result.status == 409) {
+      alert("A user with this email already exists");
+    } else {
+      alert("Unknown error: " + result.statusText);
+    }
   };
 
   return (
