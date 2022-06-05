ab8a97a596d48586fecdea5b0d2eb5566a27f95f
Timotej Šulík <xsulik@fi.muni.cz>
2022-06-05 03:42:42 +0200

    feat(frontend): Login proof of concept

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -3,18 +3,24 @@ import { Link, useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { accessTokenAtom } from "../../state/atom";
 import { AuthForm, IFormAuthInput } from "./AuthForm";
+import { authControllerLogin } from "../../api/authentication/authentication";
 
 export const Login = () => {
   const setToken = useSetRecoilState(accessTokenAtom);
   const navigate = useNavigate();
 
-  const onSubmit: SubmitHandler<IFormAuthInput> = (data: IFormAuthInput) => {
-    console.log(data);
-    window.alert("you would be logged in");
-
-    setToken("tokenikTODO");
-
-    navigate("/");
+  const onSubmit: SubmitHandler<IFormAuthInput> = async (
+    data: IFormAuthInput
+  ) => {
+    const result = await authControllerLogin({ ...data });
+    if (result.status == 201) {
+      setToken(result.data.access_token);
+      navigate("/");
+    } else if (result.status == 401) {
+      alert("Incorrect email or password!");
+    } else {
+      alert("Unknown error: " + result.statusText);
+    }
   };
 
   return (
