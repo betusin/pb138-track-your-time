27f7a4a06969c32ff9343942794ea261559d723d
Alžbeta Hajná <xhajna@fi.muni.cz>
2022-06-06 01:15:22 +0200

    feat: component message for success and fail

---
+++ frontend/src/components/Messages.tsx
@@ -0,0 +1,31 @@
+import { ErrorOutline, CheckCircleOutlined } from "@mui/icons-material";
+
+interface IMessagesProps {
+  text: string;
+}
+
+export const MessageFailBlock = ({ text }: IMessagesProps) => {
+  return (
+    <div className="m1">
+      <div className="flex-center">
+        <div className="flex-center message message--error">
+          <ErrorOutline color="error" />
+          &nbsp;{text}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const MessageSuccessBlock = ({ text }: IMessagesProps) => {
+  return (
+    <div className="m1">
+      <div className="flex-center">
+        <div className="flex-center message message--success">
+          <CheckCircleOutlined color="success" />
+          &nbsp;{text}
+        </div>
+      </div>
+    </div>
+  );
+};
