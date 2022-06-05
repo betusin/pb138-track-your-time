9552895f6787b216543717189eec6966e6ae26c7
Timotej Šulík <xsulik@fi.muni.cz>
2022-06-05 03:34:35 +0200

    feat(frontend): Configure axios with localhost backend URL

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot } from "recoil";
 import { RouterLinks } from "./RouterLinks";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3000/api/v1/";
+axios.defaults.validateStatus = () => true;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
