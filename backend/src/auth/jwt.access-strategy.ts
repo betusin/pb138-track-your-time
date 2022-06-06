1514b6d01285594ac51d063d4da116bac248fb45
xhercko <xhercko@fi.muni.cz>
2022-06-06 15:51:55 +0200

    fix(auth): remove jwt refresh strategy implemention from jwt.access-strategy.ts

--- backend/src/auth/jwt.access-strategy.ts
+++ backend/src/auth/jwt.access-strategy.ts
@@ -21,22 +21,3 @@ export class JwtAccessStrategy extends PassportStrategy(Strategy) {
     return { userId: payload.userId };
   }
 }
-
-@Injectable()
-export class JwtRefreshStrategy extends PassportStrategy(Strategy) {
-  constructor() {
-    /* https://github.com/mikenicholson/passport-jwt#configure-strategy */
-    super({
-      /* https://github.com/mikenicholson/passport-jwt#extracting-the-jwt-from-the-request */
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: process.env.JWT_REFRESH_SECRET,
-    });
-  }
-
-  /* since a 'user' object will be inserted into the request based on the return of this function,
-  it is possible to return any data needed */
-  async validate(payload: any): Promise<CurrentUser> {
-    return { userId: payload.userId };
-  }
-}
