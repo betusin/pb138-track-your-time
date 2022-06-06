15fd861a3ff4e9b408ed05c39f065be128aad30d
Alžbeta Hajná <493272@mail.muni.cz>
2022-06-06 17:10:57 +0200

    build: vite port to not collide with docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 4000,
     watch: {
       usePolling: true
+    },
+    hmr: {
+      host: 'localhost'
     }
   }
 })
