39b282d557874bdc3956abc721d9cbfa77c66131
Alžbeta Hajná <xhajna@fi.muni.cz>
2022-06-08 00:02:24 +0200

    feat: editing project

--- frontend/src/components/EditProject.tsx
+++ frontend/src/components/EditProject.tsx
@@ -1,45 +1,84 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { projectsData } from "../static/projects";
+import { useNavigate, useParams } from "react-router-dom";
+import { useRecoilValue } from 'recoil';
+import { projectControllerFindOne, projectControllerUpdate } from '../api/projects/projects';
+import { accessTokenAtom } from '../state/atom';
 import { IFormProjectInput } from "./CreateProject";
+import { failedValidationText, MessageFailBlock, MessageSuccessBlock, unauthorizedText } from './Messages';
 import { Navbar } from "./Navbar";
 import { ProjectFormElems } from "./ProjectFormElems";
 
 export const EditProject = () => {
-  const projectData = projectsData[3]; // TODO fetch the real data
-
+  const [updated, setUpdated] = useState(false);
   const [resetedForm, setResetedForm] = useState(false);
   const navigate = useNavigate();
+  const token = useRecoilValue(accessTokenAtom);
+  const { register, handleSubmit, formState, reset } = useForm<IFormProjectInput>();
+  const header = {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    }
+  };
+  const [errorMessage, setErrorMessage] = useState<string>();
 
-  const { register, handleSubmit, formState, reset } =
-    useForm<IFormProjectInput>();
+  const { id: projectID } = useParams();
+  if (!projectID) {
+    setErrorMessage("No project id, cannot retrieve the data!");
+  }
 
-  const onSubmit: SubmitHandler<IFormProjectInput> = (
+  useEffect(() => {
+    async function getProject() {
+      const result = await projectControllerFindOne(projectID, header);
+      if (result.status == 200) {
+        if (!resetedForm) {
+          reset(result.data);
+          setResetedForm(true);
+        }
+      } else if (result.status == 401) {
+        setErrorMessage(unauthorizedText);
+      } else if (result.status == 404) {
+        setErrorMessage("Project was not found!")
+      }
+    }
+    getProject();
+  }, [])
+
+  const onSubmit: SubmitHandler<IFormProjectInput> = async (
     data: IFormProjectInput
   ) => {
-    console.log(data);
-    window.alert("data would be overwritten");
-
-    navigate("/");
+    delete data.userId;
+    delete data.id;
+    const result = await projectControllerUpdate(projectID, data, header);
+    if (result.status == 200) {
+      setUpdated(true);
+      setTimeout(() => {
+        navigate('/')
+      }, 1500)
+    } else if (result.status == 400) {
+      setErrorMessage(failedValidationText);
+    } else if (result.status == 401) {
+      setErrorMessage(unauthorizedText);
+    } else if (result.status == 404) {
+      setErrorMessage("Project was not found!")
+    }
   };
-
-  if (!resetedForm) {
-    console.log(projectData);
-    reset(projectData);
-    setResetedForm(true);
-  }
 
   return (
     <div className="App">
       <Navbar />
-      <form className="m1" onSubmit={handleSubmit(onSubmit)}>
-        <ProjectFormElems
-          formState={formState}
-          register={register}
-          buttonText="Edit project"
-        />
-      </form>
+      { errorMessage && <MessageFailBlock text={errorMessage} /> }
+      { updated ?
+        <MessageSuccessBlock text="Project was successfully edited." />
+        :
+        <form className="m1" onSubmit={handleSubmit(onSubmit)}>
+          <ProjectFormElems
+            formState={formState}
+            register={register}
+            buttonText="Edit project"
+          />
+        </form>
+      }
     </div>
   );
 };
