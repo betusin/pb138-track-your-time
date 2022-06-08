a807a2398edef5a2e90fc3209161f875806a953e
Alžbeta Hajná <xhajna@fi.muni.cz>
2022-06-08 11:03:35 +0200

    fix: not displaying project page if error

--- frontend/src/components/Project.tsx
+++ frontend/src/components/Project.tsx
@@ -48,59 +48,56 @@ export const Project = () => {
   return (
     <>
       <div className="project-container">
-        { errorMessage && <MessageFailBlock text={errorMessage} /> }
-        <div>
-          <h2>{project?.name}</h2>
-        </div>
-        <div>
-          <h3>{project?.customer}</h3>
-        </div>
-        <div className="session-list">
-          {sessions.map((session) => (
-            <SessionItem
-              key={session.id}
-              session={session}
-              onRemove={removeSession}
-            />
-          ))}
-        </div>
-        <div className="btn-wrapper">
-          <Link
-            to="/session/add"
-            className="btn btn-add-circle btn-add-circle--small"
-            title="Add session"
-          >
-            +
-          </Link>
-        </div>
-        <div className="m1">
-          <table className="project-summary">
-            <thead>
-              <tr>
-                <th className="project-summary__th">{project?.hourlyRate} $/hour</th>
-                <th className="project-summary__th">Hours</th>
-                <th className="project-summary__th">Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="project-summary__td">Not invoiced yet</td>
-                <td className="project-summary__td">0</td>
-                <td className="project-summary__td">0 $</td>
-              </tr>
-              <tr>
-                <td className="project-summary__td">Invoiced</td>
-                <td className="project-summary__td">0</td>
-                <td className="project-summary__td">0 $</td>
-              </tr>
-              <tr>
-                <td className="project-summary__td">Total</td>
-                <td className="project-summary__td">0</td>
-                <td className="project-summary__td">0 $</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        { errorMessage ? <MessageFailBlock text={errorMessage} />
+        :
+        <><div>
+            <h2>{project?.name}</h2>
+          </div><div>
+              <h3>{project?.customer}</h3>
+            </div><div className="session-list">
+              {sessions.map((session) => (
+                <SessionItem
+                  key={session.id}
+                  session={session}
+                  onRemove={removeSession} />
+              ))}
+            </div><div className="btn-wrapper">
+              <Link
+                to="/session/add"
+                className="btn btn-add-circle btn-add-circle--small"
+                title="Add session"
+              >
+                +
+              </Link>
+            </div><div className="m1">
+              <table className="project-summary">
+                <thead>
+                  <tr>
+                    <th className="project-summary__th">{project?.hourlyRate} $/hour</th>
+                    <th className="project-summary__th">Hours</th>
+                    <th className="project-summary__th">Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="project-summary__td">Not invoiced yet</td>
+                    <td className="project-summary__td">0</td>
+                    <td className="project-summary__td">0 $</td>
+                  </tr>
+                  <tr>
+                    <td className="project-summary__td">Invoiced</td>
+                    <td className="project-summary__td">0</td>
+                    <td className="project-summary__td">0 $</td>
+                  </tr>
+                  <tr>
+                    <td className="project-summary__td">Total</td>
+                    <td className="project-summary__td">0</td>
+                    <td className="project-summary__td">0 $</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div></>
+        }
       </div>
     </>
   );
