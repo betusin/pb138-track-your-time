f77da5130a5e4715cf21412a090f10617a28f74c
Alžbeta Hajná <xhajna@fi.muni.cz>
2022-06-08 13:32:20 +0200

    feat: creating and editing sessions possible

--- frontend/src/components/CreateSession.tsx
+++ frontend/src/components/CreateSession.tsx
@@ -1,45 +1,126 @@
+import { DateTimePicker, LocalizationProvider } from '@mui/lab';
+import { TextField } from '@mui/material';
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { SessionFormElems } from "./SessionFormElems";
-
-export interface IFormSessionInput {
-  fromDate: Date;
-  toDate: Date;
-  isInvoiced: boolean;
-  hourly_rate: number;
-  note: string;
-}
-
-const blankSession = {
-  fromDate: new Date(),
-  toDate: new Date(),
-  isInvoiced: false,
-  hourly_rate: 0, // TODO hourly_rate from poject
-  note: "",
-};
+import { useNavigate, useParams } from "react-router-dom";
+import { useRecoilValue } from 'recoil';
+import { CreateSessionDto } from '../api/model';
+import { projectControllerCreateSession } from '../api/sessions/sessions';
+import { accessTokenAtom } from '../state/atom';
+import { useEffect, useState } from 'react';
+import { failedValidationText, MessageFailBlock, MessageSuccessBlock, unauthorizedText } from './Messages';
+import { format } from 'date-fns';
+import { projectControllerFindOne } from '../api/projects/projects';
 
 export const CreateSession = () => {
   const navigate = useNavigate();
-  const projectID = "randomID";
+  const { projectId } = useParams();
+  const [ hourlyRate, setHourlyRate] = useState(0);
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [successMessage, setSuccessMessage] = useState<string>();
+  const [toDate, setToDate] = useState(new Date());
+  const [fromDate, setFromDate] = useState(new Date());
+  const token = useRecoilValue(accessTokenAtom);
+  const header = {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    }
+  };
+  const { register, handleSubmit, formState } = useForm<CreateSessionDto>();
 
-  const { register, handleSubmit, formState } = useForm<IFormSessionInput>();
+  useEffect(() => {
+    async function getProjectHourlyRate() {
+      if (projectId != null) {
+        const result = await projectControllerFindOne(projectId, header);
+        if (result.status == 200) {
+          setHourlyRate(result.data.hourlyRate);
+        } else if (result.status == 401) {
+          setErrorMessage(unauthorizedText);
+        } else if (result.status == 404) {
+          setErrorMessage("Project was not found!");
+        }
+      }
+    }
+    getProjectHourlyRate();
+  }, []);
 
-  const onSubmit = (data: IFormSessionInput) => {
-    console.log(data);
-    window.alert("new session would be created with data: ");
-
-    navigate("/project/" + projectID);
+  const onSubmit = async (data: CreateSessionDto) => {
+    data.toDate = format(toDate, "yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
+    data.fromDate = format(fromDate, "yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
+    const result = await projectControllerCreateSession(projectId!, data, header);
+    if (result.status == 201) {
+      setSuccessMessage("Session was created successfully.");
+      setTimeout(() => {
+        navigate("/project/" + projectId);
+      }, 1500)
+    } else if (result.status == 400) {
+      setErrorMessage(failedValidationText);
+    } else if (result.status == 401) {
+      setErrorMessage(unauthorizedText);
+    }
   };
 
   return (
     <>
+      { errorMessage && <MessageFailBlock text={errorMessage} /> }
+      { successMessage && <MessageSuccessBlock text={successMessage} /> }
       <form className="m1" onSubmit={handleSubmit(onSubmit)}>
-        <SessionFormElems
-          formState={formState}
-          register={register}
-          sessionData={blankSession}
-          buttonText="Create session"
+        <div className="form-dates">
+          <div className="form-dates__picker">
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DateTimePicker
+                renderInput={(props) => <TextField {...props} />}
+                label="DateTimePicker"
+                value={fromDate}
+                {...register("fromDate")}
+                onChange={(newValue) => {
+                  if (newValue != null) {
+                    setFromDate(newValue);
+                  }
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+          <div className="form-dates__picker">
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DateTimePicker
+                renderInput={(props) => <TextField {...props} />}
+                label="DateTimePicker"
+                value={toDate}
+                {...register("toDate")}
+                onChange={(newValue) => {
+                  if (newValue != null) {
+                    setToDate(newValue);
+                  }
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+        </div>
+
+        <div>
+          <label>Hourly rate</label>
+        </div>
+        <input
+          className={`number-field`}
+          type="number"
+          step={10}
+          min={0}
+          defaultValue={hourlyRate}
+          {...register("hourlyRate", { valueAsNumber: true })}
         />
+
+        <div>
+          <label>Note</label>
+        </div>
+        <textarea
+          className={`text-field ${formState.errors.note && "text-field--error"}`}
+          {...register("note")}
+        />
+
+        <div className="btn-wrapper">
+          <input className="btn btn--primary" type="submit" value="Create session" />
+        </div>
       </form>
     </>
   );
--- frontend/src/components/EditSession.tsx
+++ frontend/src/components/EditSession.tsx
@@ -1,33 +1,138 @@
+import { DateTimePicker, LocalizationProvider } from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { Checkbox, FormControlLabel, TextField } from '@mui/material';
+import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { sessionData } from "../static/sessions";
-import { IFormSessionInput } from "./CreateSession";
-import { SessionFormElems } from "./SessionFormElems";
+import { useNavigate, useParams } from "react-router-dom";
+import { useRecoilValue } from 'recoil';
+import { GetSessionDto, UpdateSessionDto } from '../api/model';
+import { sessionControllerFindOne, sessionControllerUpdate } from '../api/sessions/sessions';
+import { accessTokenAtom } from '../state/atom';
+import { failedValidationText, MessageFailBlock, MessageSuccessBlock, unauthorizedText } from './Messages';
 
 export const EditSession = () => {
   const navigate = useNavigate();
-  const projectID = "randomID";
+  const { projectId, sessionId } = useParams();
+  const [session, setSession] = useState<GetSessionDto>();
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [successMessage, setSuccessMessage] = useState<string>();
+  const [toDate, setToDate] = useState(new Date());
+  const [fromDate, setFromDate] = useState(new Date());
+  const token = useRecoilValue(accessTokenAtom);
+  const header = {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    }
+  };
+  const { register, handleSubmit, formState } = useForm<UpdateSessionDto>();
 
-  const session = sessionData[0];
+  useEffect(() => {
+    async function getSession() {
+      const result = await sessionControllerFindOne(sessionId!, header);
+      if (result.status == 200) {
+        setSession(result.data);
+        setFromDate(new Date(result.data.fromDate));
+        setToDate(new Date(result.data.toDate));
+      } else if (result.status == 401) {
+        setErrorMessage(unauthorizedText);
+      } else if (result.status == 404) {
+        setErrorMessage('Session was not found');
+      }
+    }
+    getSession();
+  }, []);
 
-  const { register, handleSubmit, formState } = useForm<IFormSessionInput>();
-
-  const onSubmit = (data: IFormSessionInput) => {
-    console.log(data);
-    window.alert("new session would be created with data: ");
-
-    navigate("/project/" + projectID);
+  const onSubmit = async (data: UpdateSessionDto) => {
+    data.toDate = format(toDate, "yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
+    data.fromDate = format(fromDate, "yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
+    const result = await sessionControllerUpdate(sessionId!, data, header);
+    if (result.status == 200) {
+      setSuccessMessage("Session was updated successfully.");
+      setTimeout(() => {
+        navigate("/project/" + projectId);
+      }, 1500)
+    } else if (result.status == 400) {
+      setErrorMessage(failedValidationText);
+    } else if (result.status == 401) {
+      setErrorMessage(unauthorizedText);
+    } else if (result.status == 404) {
+      setErrorMessage('Session was not found');
+    }
   };
 
   return (
     <>
+      { errorMessage && <MessageFailBlock text={errorMessage} /> }
+      { successMessage && <MessageSuccessBlock text={successMessage} /> }
       <form className="m1" onSubmit={handleSubmit(onSubmit)}>
-        <SessionFormElems
-          formState={formState}
-          register={register}
-          sessionData={session}
-          buttonText="Edit session"
+        <div className="form-dates">
+          <div className="form-dates__picker">
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DateTimePicker
+                renderInput={(props) => <TextField {...props} />}
+                label="DateTimePicker"
+                value={fromDate}
+                {...register("fromDate")}
+                onChange={(newValue) => {
+                  if (newValue != null) {
+                    setFromDate(newValue);
+                  }
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+          <div className="form-dates__picker">
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DateTimePicker
+                renderInput={(props) => <TextField {...props} />}
+                label="DateTimePicker"
+                value={toDate}
+                {...register("toDate")}
+                onChange={(newValue) => {
+                  if (newValue != null) {
+                    setToDate(newValue);
+                  }
+                }}
+              />
+            </LocalizationProvider>
+          </div>
+        </div>
+
+        <FormControlLabel
+          control={
+            <Checkbox
+              {...register("isInvoiced")}
+              defaultChecked={session?.isInvoiced}
+            />
+          }
+          label="is invoiced"
         />
+
+        <div>
+          <label>Hourly rate</label>
+        </div>
+        <input
+          className={`number-field`}
+          type="number"
+          step={10}
+          min={0}
+          defaultValue={session?.hourlyRate ? session?.hourlyRate : 0}
+          {...register("hourlyRate", { valueAsNumber: true })}
+        />
+
+        <div>
+          <label>Note</label>
+        </div>
+        <textarea
+          className={`text-field ${formState.errors.note && "text-field--error"}`}
+          defaultValue={session?.note}
+          {...register("note")}
+        />
+
+        <div className="btn-wrapper">
+          <input className="btn btn--primary" type="submit" value="Update session" />
+        </div>
       </form>
     </>
   );
