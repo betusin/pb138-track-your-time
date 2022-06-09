c8e231ca795bb5c2800e3aec5f0173fc69f71156
Timotej Šulík <xsulik@fi.muni.cz>
2022-06-09 23:36:25 +0200

    refactor(frontend): Remove unused operation result components

--- frontend/src/components/Messages.tsx
+++ frontend/src/components/Messages.tsx
@@ -1,22 +1,3 @@
-import { CheckCircleOutlined, ErrorOutline } from "@mui/icons-material";
-
-interface IMessagesProps {
-  text: string;
-}
-
-export const MessageFailBlock = ({ text }: IMessagesProps) => {
-  return (
-    <div className="m1">
-      <div className="flex-center">
-        <div className="flex-center message message--error">
-          <ErrorOutline color="error" />
-          &nbsp;{text}
-        </div>
-      </div>
-    </div>
-  );
-};
-
 export const unauthorizedText = "Unauthorized operation!";
 
 export const failedValidationText = "Field validation failed!";
@@ -38,16 +19,3 @@ export const sessionDeletedText = "Session deleted successfully.";
 export const sessionCreatedText = "Session was successfully created.";
 
 export const sessionUpdatedText = "Session was successfully updated.";
-
-export const MessageSuccessBlock = ({ text }: IMessagesProps) => {
-  return (
-    <div className="m1">
-      <div className="flex-center">
-        <div className="flex-center message message--success">
-          <CheckCircleOutlined color="success" />
-          &nbsp;{text}
-        </div>
-      </div>
-    </div>
-  );
-};
