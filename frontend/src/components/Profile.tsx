ab1b8550786f3ec4267473abed2d420bc4167c1f
xhercko <xhercko@fi.muni.cz>
2022-06-09 16:19:50 +0200

    feat(profile): Initial profile component

---
+++ frontend/src/components/Profile.tsx
@@ -0,0 +1,13 @@
+export const Profile = () => {
+  return <>
+    <div className="profile-header">
+      <h1>Profile</h1>
+      <img src="/assets/edit-lime.svg" alt="edit" />
+    </div>
+    <img src="logo" alt="logo" />
+    <span>Name surname</span>
+    <span>Company name</span>
+    <span>email</span>
+    <button>Sign out</button>
+  </>;
+};
