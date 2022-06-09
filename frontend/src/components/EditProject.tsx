0094f96057134d5a29ddd9fee6268be1f0dd317a
Timotej Šulík <xsulik@fi.muni.cz>
2022-06-09 13:56:59 +0200

    refactor(frontend): Reimplement EditProject fetching using SWR

--- frontend/src/components/EditProject.tsx
+++ frontend/src/components/EditProject.tsx
@@ -1,13 +1,12 @@
-import { AxiosResponse } from "axios";
 import { useEffect } from "react";
-import { SubmitHandler, useForm, UseFormSetValue } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { useNavigate, useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
-import { GetProjectDto, UpdateProjectDto } from "../api/model";
+import { UpdateProjectDto } from "../api/model";
 import {
-  projectControllerFindOne,
   projectControllerUpdate,
+  useProjectControllerFindOne,
 } from "../api/projects/projects";
 import { accessTokenAtom } from "../state/atom";
 import { IFormProjectInput } from "./CreateProject";
@@ -18,25 +17,7 @@ import {
   unexpectedErrorText,
 } from "./Messages";
 import { ProjectFormElems } from "./ProjectFormElems";
-
-function onProjectReceived(
-  result: AxiosResponse<GetProjectDto>,
-  setValue: UseFormSetValue<IFormProjectInput>
-) {
-  if (result.status == 200) {
-    if (result.data.customer != null)
-      setValue("customer", result.data.customer);
-    setValue("hourlyRate", result.data.hourlyRate);
-    setValue("isActive", result.data.isActive);
-    setValue("name", result.data.name);
-  } else if (result.status == 401) {
-    toast.error(unauthorizedText);
-  } else if (result.status == 404) {
-    toast.error(noProjectFoundText);
-  } else {
-    toast.error(unexpectedErrorText);
-  }
-}
+import { useApiSwrCall } from "../util/api-caller";
 
 export const EditProject = () => {
   const navigate = useNavigate();
@@ -48,15 +29,20 @@ export const EditProject = () => {
       Authorization: "Bearer " + token,
     },
   };
-
   const { id: projectIDParam } = useParams();
   const projectID = projectIDParam ?? "";
-
+  const { data } = useApiSwrCall((o) => {
+    return useProjectControllerFindOne(projectID, o);
+  });
   useEffect(() => {
-    projectControllerFindOne(projectID, header)
-      .then((result) => onProjectReceived(result, setValue))
-      .catch(() => toast.error(unexpectedErrorText));
-  }, []);
+    if (data?.status == 404) toast.error(noProjectFoundText);
+    if (data?.data.customer) {
+      setValue("customer", data.data.customer);
+      setValue("hourlyRate", data.data.hourlyRate);
+      setValue("isActive", data.data.isActive);
+      setValue("name", data.data.name);
+    }
+  }, [data]);
 
   const onSubmit: SubmitHandler<IFormProjectInput> = async (
     data: IFormProjectInput
