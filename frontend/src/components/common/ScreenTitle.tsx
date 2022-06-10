eb059326c3174b89dfbd5e1fcf32dd9cc2971824
Timotej Šulík <xsulik@fi.muni.cz>
2022-06-10 03:34:24 +0200

    feat(frontend): Screen titles

---
+++ frontend/src/components/common/ScreenTitle.tsx
@@ -0,0 +1,29 @@
+import { Stack, Typography } from "@mui/material";
+
+export interface ScreenTitleProps {
+  title: string;
+  secondaryTitle?: string;
+}
+
+export function ScreenTitle({ title, secondaryTitle }: ScreenTitleProps) {
+  return (
+    <Stack
+      className="screen-title"
+      direction="row"
+      flexWrap="wrap"
+      justifyContent="space-between"
+      alignItems="center"
+      p={2}
+      px={0.5}
+    >
+      <Typography variant="h6" p={2} px={3}>
+        {title}
+      </Typography>
+      {secondaryTitle && (
+        <Typography variant="body2" p={2} px={3}>
+          {secondaryTitle}
+        </Typography>
+      )}
+    </Stack>
+  );
+}
