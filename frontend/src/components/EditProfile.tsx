4910b007986564167018b713a446f7f79480684d
xhercko <xhercko@fi.muni.cz>
2022-06-10 11:38:23 +0200

    style: apply prettier

--- frontend/src/components/EditProfile.tsx
+++ frontend/src/components/EditProfile.tsx
@@ -1,3 +1,3 @@
 export const EditProfile = () => {
   return <></>;
-}
+};
--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,13 +1,15 @@
 export const Profile = () => {
-  return <>
-    <div className="profile-header">
-      <h1>Profile</h1>
-      <img src="/assets/edit-lime.svg" alt="edit" />
-    </div>
-    <img src="logo" alt="logo" />
-    <span>Name surname</span>
-    <span>Company name</span>
-    <span>email</span>
-    <button>Sign out</button>
-  </>;
+  return (
+    <>
+      <div className="profile-header">
+        <h1>Profile</h1>
+        <img src="/assets/edit-lime.svg" alt="edit" />
+      </div>
+      <img src="logo" alt="logo" />
+      <span>Name surname</span>
+      <span>Company name</span>
+      <span>email</span>
+      <button>Sign out</button>
+    </>
+  );
 };
