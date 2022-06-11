18efa69a1040bca3987724c31ed6d47c3f18d019
xhercko <xhercko@fi.muni.cz>
2022-06-11 18:46:14 +0200

    feat(profile): style reworked with new ui redesign

--- frontend/src/components/profile/Profile.tsx
+++ frontend/src/components/profile/Profile.tsx
@@ -7,6 +7,9 @@ import { useApiCall } from "../../util/api-caller";
 import { useLoadProfile } from "../../util/load-entity-wrappers";
 import { LoadingPlaceholder } from "../common/LoadingPlaceholder";
 import "../../styles/Profile.css";
+import { ScreenTitle } from "../common/ScreenTitle";
+import i18n from "../../i18n/i18n";
+import { Stack } from "@mui/material";
 
 export const Profile = () => {
   const doApiCall = useApiCall();
@@ -34,43 +37,45 @@ export const Profile = () => {
   };
 
   return (
-    <div className="profile">
-      <div className="profile-header">
-        <h1 className="profile-header__title">
-          <Trans i18nKey="app.profile" />
-        </h1>
-        <Link to="/me/edit">
-          <img
-            className="icon profile-header__icon"
-            src="/assets/edit-lime.svg"
-            alt="edit"
-          />
-        </Link>
+    <>
+      <ScreenTitle title={i18n.t("app.profile")} />
+      <div className="profile-wrapper">
+        <Stack>
+          <Link className="icon-wrapper" to="/me/edit">
+            <img
+              className="icon profile-header__icon"
+              src="/assets/edit-lime.svg"
+              alt="edit"
+            />
+          </Link>
+          <img className="profile__logo" src={profile.logo} alt="logo" />
+          <div className="profile-field profile__name">
+            <p className="profile-field__label">
+              <Trans i18nKey="profile.name" />
+            </p>
+            <p className="profile-field__value">
+              {profile.name} {profile.surname}
+            </p>
+          </div>
+          <div className="profile-field profile__company">
+            <p className="profile-field__label">
+              <Trans i18nKey="profile.company_name" />
+            </p>
+            <p className="profile-field__value">{profile.company}</p>
+          </div>
+          <div className="profile-field profile__email">
+            <p className="profile-field__label">
+              <Trans i18nKey="profile.email" />
+            </p>
+            <p className="profile-field__value">{profile.email}</p>
+          </div>
+          <div className="btn-wrapper">
+            <button className="btn btn--primary" onClick={signOut}>
+              <Trans i18nKey="profile.sign_out" />
+            </button>
+          </div>
+        </Stack>
       </div>
-      <img className="profile__logo" src={profile.logo} alt="logo" />
-      <div className="profile-field profile__name">
-        <p className="profile-field__label">
-          <Trans i18nKey="profile.name" />
-        </p>
-        <p className="profile-field__value">
-          {profile.name} {profile.surname}
-        </p>
-      </div>
-      <div className="profile-field profile__company">
-        <p className="profile-field__label">
-          <Trans i18nKey="profile.company_name" />
-        </p>
-        <p className="profile-field__value">{profile.company}</p>
-      </div>
-      <div className="profile-field profile__email">
-        <p className="profile-field__label">
-          <Trans i18nKey="profile.email" />
-        </p>
-        <p className="profile-field__value">{profile.email}</p>
-      </div>
-      <button className="btn btn--primary" onClick={signOut}>
-        <Trans i18nKey="profile.sign_out" />
-      </button>
-    </div>
+    </>
   );
 };
