211d95d648d9bad2db5b26dc99f1159aae6f865c
Timotej Šulík <xsulik@fi.muni.cz>
2022-06-12 00:32:14 +0200

    feat(frontend): Proper styling for session forms

---
+++ frontend/src/components/session/SessionForm.tsx
@@ -0,0 +1,133 @@
+import { Checkbox } from "@mui/material";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import { Controller, useForm } from "react-hook-form";
+import { IFormSessionInput } from "./CreateSession";
+import { DateTimePicker } from "@mui/lab";
+import { StyledTextField } from "../common/StyledTextField";
+import { theme } from "../../styles/theme";
+import { Trans } from "react-i18next";
+import { GetSessionDto } from "../../api/model";
+
+export interface SessionFormElemsProps {
+  buttonText: string;
+  prefill?: GetSessionDto;
+  fallbackHourlyRate: number;
+  onSubmit: (data: IFormSessionInput) => void;
+}
+
+function parseOrNull(date?: string): Date | undefined {
+  if (!date) {
+    return undefined;
+  }
+  return new Date(date);
+}
+
+export const SessionForm = ({
+  buttonText,
+  prefill,
+  fallbackHourlyRate,
+  onSubmit,
+}: SessionFormElemsProps) => {
+  const { register, handleSubmit, formState, control } =
+    useForm<IFormSessionInput>({
+      defaultValues: {
+        fromDate: parseOrNull(prefill?.fromDate) ?? new Date(),
+        toDate: parseOrNull(prefill?.toDate) ?? new Date(),
+        isInvoiced: prefill?.isInvoiced ?? false,
+        hourlyRate: prefill?.hourlyRate ?? fallbackHourlyRate,
+        note: prefill?.note ?? "",
+      },
+    });
+  return (
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <div className="form--inner-container">
+        <div className="form--adjacent-group">
+          <div className="form--field">
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <Controller
+                name="fromDate"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <DateTimePicker
+                    renderInput={StyledTextField}
+                    label={<Trans i18nKey="session.date_from" />}
+                    value={value}
+                    onChange={(value) => {
+                      onChange(value);
+                    }}
+                  />
+                )}
+              />
+            </LocalizationProvider>
+          </div>
+          <div className="form--field">
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <Controller
+                name="toDate"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <DateTimePicker
+                    renderInput={StyledTextField}
+                    label={<Trans i18nKey="session.date_to" />}
+                    value={value}
+                    onChange={onChange}
+                  />
+                )}
+              />
+            </LocalizationProvider>
+          </div>
+        </div>
+
+        <div className="form--adjacent-group">
+          <div className="form--field">
+            <label htmlFor="isInvoiced">
+              <Trans i18nKey="session.is_invoiced" />
+            </label>
+            <div>
+              <Checkbox
+                id="isInvoiced"
+                {...register("isInvoiced")}
+                style={{ color: theme.palette.secondary.light }}
+              />
+            </div>
+          </div>
+
+          <div className="form--field">
+            <label>
+              <Trans i18nKey="session.hourly_rate" />
+            </label>
+            <input
+              className={`number-field`}
+              type="number"
+              min={0}
+              {...register("hourlyRate", { valueAsNumber: true })}
+            />
+          </div>
+        </div>
+
+        <div className="form--field">
+          <div>
+            <label>
+              <Trans i18nKey="session.note" />
+            </label>
+          </div>
+          <textarea
+            className={`text-field form-textarea ${
+              formState.errors.note && "text-field--error"
+            }`}
+            {...register("note")}
+          />
+        </div>
+
+        <div className="btn-wrapper">
+          <input
+            className="btn btn--primary"
+            type="submit"
+            value={buttonText}
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
