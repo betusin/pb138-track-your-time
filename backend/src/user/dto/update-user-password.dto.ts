ac95c9cc4f51bec7a9ead489d6e7c92b441bba6a
xhercko <xhercko@fi.muni.cz>
2022-06-14 11:48:42 +0200

    fix(backend): change password dto requires non empty passwords

--- backend/src/user/dto/update-user-password.dto.ts
+++ backend/src/user/dto/update-user-password.dto.ts
@@ -1,12 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class UpdateUserPasswordDto {
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   oldPassword: string;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   newPassword: string;
 }
